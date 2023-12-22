e8d025739cd32d7d75246d3cfaa53c7ebbe7de21
Børge Bjørnstadjordet <borgetull@gmail.com>
2023-12-22 21:01:36 +0100

    Kattis 2

--- kattis/easy/undeadoralive.java
+++ kattis/easy/undeadoralive.java
@@ -1,4 +1,23 @@
 package easy;
 
+import java.util.Scanner;
+
 public class undeadoralive {
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			String line = scanner.nextLine();
+
+			int smile = 0;
+			int frown = 0;
+
+			if (line.contains(":)")) smile = 1;
+			if (line.contains(":(")) frown = 1;
+
+			if (smile == 1 && frown == 1) System.out.println("double agent");
+			else if (smile == 1) System.out.println("alive");
+			else if (frown == 1) System.out.println("undead");
+			else System.out.println("machine");
+		}
+	}
 }
