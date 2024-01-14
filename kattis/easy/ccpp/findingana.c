3a92e2acfa9614239c2672e154eb3ee020b0f3b8
Børge Bjørnstadjordet <borgetull@gmail.com>
2024-01-14 05:39:22 +0100

    Create findingana.c

---
+++ kattis/easy/ccpp/findingana.c
@@ -0,0 +1,20 @@
+//
+// Created by BBJ on 14.01.2024.
+//
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char line[1000], copy[1000];
+    scanf("%1000s", line);
+
+    // pointer to first occurrence
+    char *occ = strchr(line, 'a');
+
+//     prints string from first occurrence (until null terminator)
+    while (*occ != '\0') {
+        printf("%c", *occ++);
+    }
+
+    return 0;
+}
